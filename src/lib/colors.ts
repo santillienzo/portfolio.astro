1619152022631ddd00f3e0d3805ce02d9f7e69be
santillienzo <enzo.135246@gmail.com>
2024-07-22 17:06:35 -0300

    feat: make intro and navbar

---
+++ src/lib/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+    backgroundColor: "#F5F4E1",
+    primaryColor: {
+        light: "#7C9573",
+        dark: "#2D403A",
+    },
+    secondaryColor: {
+        light: "#DFBB91",
+        dark: "#341E11",
+    },
+    textColor: {
+        light: "#f8f8f7",
+        dark: "#231B1A",
+    },
+    linkColor: {
+        light: "#f5f4e9",
+        dark: "#AF5E33",
+    },
+} 
